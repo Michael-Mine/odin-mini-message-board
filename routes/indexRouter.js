4812004d95f33cf8ac2ca2c88e1774ba17b87453
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-02-05 22:16:09 +0000

    feat: add index and new views

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
-
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => res.send("Home"));
+indexRouter.get("/", (req, res) => res.render("index"));
+indexRouter.get("/new", (req, res) => res.render("new"));
 
 module.exports = indexRouter;

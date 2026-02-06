cc8e8efc8585767d5d98268cc4e145a775044143
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-02-06 09:05:50 +0000

    feat: add messages array & add to view

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,7 +1,22 @@
 const { Router } = require("express");
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => res.render("index"));
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+indexRouter.get("/", (req, res) =>
+  res.render("index", { title: "Mini Message Board", messages: messages }),
+);
 indexRouter.get("/new", (req, res) => res.render("new"));
 
 module.exports = indexRouter;

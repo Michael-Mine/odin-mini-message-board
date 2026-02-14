1e0e0aee2c069da7914bb20a8480962794e4272b
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-02-14 19:53:57 +0000

    fix: set port back to 3000 only

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
 app.use("/", router);
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, (error) => {
   if (error) {
     throw error;

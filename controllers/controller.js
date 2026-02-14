863cb0a4e3e8722cd9b02bf2eacf64051625b424
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-02-14 22:00:09 +0000

    feat: add server-side validation for user input

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,4 +1,19 @@
 const db = require("../db/queries");
+const { body, validationResult, matchedData } = require("express-validator");
+
+const lengthErr = "must be between 1 and 20 characters.";
+const textErr = "must be between 1 and 300 characters.";
+
+const validateMessage = [
+  body("name")
+    .trim()
+    .isLength({ min: 1, max: 20 })
+    .withMessage(`Name ${lengthErr}`),
+  body("message")
+    .trim()
+    .isLength({ min: 1, max: 300 })
+    .withMessage(`Message ${textErr}`),
+];
 
 async function getAllMessages(req, res) {
   const messages = await db.getAllMessages();
@@ -14,15 +29,23 @@ function newMessageGet(req, res) {
   res.render("form", { title: "Add New Message" });
 }
 
-async function newMessagePost(req, res) {
-  const newMessage = {
-    name: req.body.name,
-    added: new Date(),
-    text: req.body.message,
-  };
-  await db.insertMessage(newMessage);
-  res.redirect("/");
-}
+const newMessagePost = [
+  validateMessage,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("form", {
+        title: "Add New Message",
+        errors: errors.array(),
+      });
+    }
+
+    const { name, message } = matchedData(req);
+    const added = new Date();
+    await db.insertMessage({ name, added, text: message });
+    res.redirect("/");
+  },
+];
 
 module.exports = {
   getAllMessages,
